d3e2f0a909d8a4e5439bc7bc6bf952ffa316a45d
Pratheesh <pratheesh1218@gmail.com>
2023-04-12 17:23:39 +0530

    initial commit

---
+++ random-quote-generator/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {useState,useEffect} from 'react';
+
+const getRandomQuote = (quotes) => {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+
+const App = () => {
+
+  const [quotes,setQuotes] = useState([]);
+  const [quote , setQuote] = useState(null);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+        .then((res) => res.json())
+        .then((json) => {
+          setQuotes(json);
+          setQuote(json[0]);
+        })
+  },[]);
+
+  const getNewQuote = () => {
+    setQuote(getRandomQuote(quotes));
+  }
+
+
+  return(
+    <main>
+      <h1>  Quote Generator </h1>
+      <section>
+        <button onClick={getNewQuote}>  New Quote </button>
+        <h3>
+          <span> " </span>
+          {quote?.text}
+        </h3>
+        <i> {quote?.author} </i>
+      </section>
+    </main>
+  );
+};
+
+export default App;
