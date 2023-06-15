e53ff7a5e04918f21cb618951324cd36d7531738
Pratheesh <pratheesh1218@gmail.com>
2023-06-15 22:15:39 +0530

    added some files

--- portfolio-own/src/App.js
+++ portfolio-own/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
-
+import {BrowserRouter as Router , Routes , Route , Link} from 'react-router-dom';
+import Home from './components/Home';
+import Contacts  from './components/Contacts';
+import About from './components/About';
 
 const App = () => {
   return(
-    <div>
-      Hello
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contacts} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ portfolio-own/src/components/Contacts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const Contacts = () => {
+    return(
+        <div>
+            Hello Contacts
+        </div>
+    );
+};
+
+
+export default Contacts;
