31ff3def4af17891f334dba28a682e15c73d8bfc
Pratheesh <pratheesh1218@gmail.com>
2023-07-27 18:44:59 +0530

    data fetching okay

--- inter-prep/src/components/Api.js
+++ inter-prep/src/components/Api.js
@@ -1,37 +1,46 @@
 import { useEffect , useState } from "react";
+import axios from 'axios';
 
 
 
 const Api = () => {
     const [loading , setLoading] = useState(true);
-    const [data,setData] = useState(null);
-
-
-    const fetchApi = () => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-           .then((response) => {
-            return response.json();
-           })
-           .then((json) => {
-            console.log(json);
-            setLoading(false);
-            setData(json);
-           });
-    };
+    const [users,setUsers] = useState(null);
 
 
     useEffect(() => {
-        fetchApi();
+        fetchUsers();
     } , []);
 
-    if(loading){
-        return <h1>  Loading </h1>;
-    }
+     const fetchUsers = () => {
+        axios
+             .get('https://jsonplaceholder.typicode.com/users')
+             .then((res) => {
+                console.log(res);
+                setLoading(false);
+                setUsers(res.data);
+             })
+             .catch((err) => {
+                console.log(err);
+             })
+     }
+
+     if(loading){
+          return <h1> Loading </h1>
+     }
+   
 
     return(
         <div>
             <h1>  Data fetched successfully </h1>
-            {JSON.stringify(data)}
+            <div className="item-container">
+                { users && users.map((user) => (
+                    <div className="card"  key={user.id} >
+                       <p>  {user.name} </p>
+                       {/* <p>  {user.email} </p> */}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 };
