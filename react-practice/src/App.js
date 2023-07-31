46b8bd808b6f048806f62e01234b2e05f65617f2
Pratheesh <pratheesh1218@gmail.com>
2023-07-31 17:39:51 +0530

    created a new project

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ToolBar from './Problem';
+import Country from './Country';
 
 
 const App = () => {
-   return(
+  return(
     <div>
-      <ToolBar /> 
+      <Country />
     </div>
-   );
+  );
 };
 
 
